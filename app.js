3e3e0df95e5e2932c851a4504426b2d23fb248ca
Oscar131-maker <57594026+Oscar131-maker@users.noreply.github.com>
2020-07-29 17:34:56 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const course = document.querySelector('#inicio-curso');
 const cedula = document.querySelector('#cedula');
 const inputSubmit = document.querySelector('#inputbtn');
 
-
+// Diploma
 inputSubmit.addEventListener('click', () => {
     
     const name_diploma = document.querySelector('#name_diploma');
@@ -17,5 +17,26 @@ inputSubmit.addEventListener('click', () => {
     
 });
 
+// Login Form
+const form_login = document.querySelector('#submit-login');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
 
+form_login.addEventListener('submit', (event) => {
+   event.preventDefault();
+   
+   //Values
+   const username_validate = "cannabis_admin";
+   const password_validate = "Soy_estudianteCertificado_01359";
+   const username_value = username.value;
+   const password_value = password.value;
+   const login_section = document.querySelector('#form-login');
+   const main = document.querySelector('#main');
+
+   if(username_value == username_validate || password_value == password_validate){
+     login_section.style.display = "none";
+     main.style.display = "block";
+   }
+
+});
 
